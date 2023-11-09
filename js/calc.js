2f90d2c4e0304b3db95fb7ee0f8353f296ffc9af
Roc327 <nate_van@hotmail.com>
2023-11-09 15:09:28 -0500

    Added some code to the operation functions and boilerplate html

---
+++ js/calc.js
@@ -0,0 +1,36 @@
+let numOne = 0;
+let numTwo = 0;
+let result = 0;
+let operatorSelection = "";
+
+function add() {
+  return arguments[0] + arguments[1];
+}
+
+function subtract() {
+  return arguments[0] - arguments[1];
+}
+
+function multiply() {
+  return arguments[0] * arguments[1];
+}
+
+function divide() {
+  return arguments[0] / arguments[1];
+}
+
+function getNumbers() {
+  // basic code to get numbers from user
+}
+
+function displayNumbers() {
+  // basic code to display number back on screen
+}
+
+function clearCalc() {
+  // clear calculator to reset all operations
+}
+
+function operatorClick() {
+  // code when an operator button is clickec
+}
