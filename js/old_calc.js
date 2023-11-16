c453a1d569da61dec1913420b939fcbc130c0026
Roc327 <nate_van@hotmail.com>
2023-11-16 08:59:32 -0500

    fleshed basic js refactor with functions and variables I wasnt to try

--- js/old_calc.js
+++ js/old_calc.js
@@ -1,8 +1,7 @@
-let currNum = "0";
-let prevNum = "0";
-let currOp = "";
-let prevOp = "";
-let result = "0";
+let storedNum = "0";
+let displayNum = "0";
+let operatorSelection = "";
+let operandNums = 0;
 
 function add() {
   return arguments[0] + arguments[1];
@@ -21,19 +20,44 @@ function divide() {
 }
 
 function onNumClick(num) {
-  //
+  if (displayNum === "0") {
+    displayNum = num;
+    displayNumber(displayNum);
+  } else {
+    displayNum += num;
+    displayNumber(displayNum);
+  }
 }
 
-function displayNumber() {
-  //
+function displayNumber(displayNum) {
+  document.getElementById("numbers").innerHTML = displayNum;
+  if (displayNum.length > 25) {
+    document.getElementById("num-display").style.fontSize = "20px";
+  } else if (displayNum.length > 15) {
+    document.getElementById("num-display").style.fontSize = "32px";
+  } else if (displayNum.length <= 15) {
+    document.getElementById("num-display").style.fontSize = "48px";
+  }
 }
 
 function clearCalc() {
-  //
+  displayNum = "0";
+  storedNum = "0";
+  operatorSelection = "";
+  operandNums = 0;
+  displayNumber(displayNum);
 }
 
 function operatorClick(operator) {
-  //
+  operatorSelection = operator;
+  if (operandNums >= 1) {
+    operate(storedNum, displayNum, operator);
+  } else {
+    storedNum = displayNum;
+    displayNum = "0";
+    operandNums++;
+    // displayNumber(displayNum);
+  }
 }
 
 function operate(firstNum, secondNum, operator) {
